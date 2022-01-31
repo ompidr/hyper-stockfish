0bc18e6d638dd0ed088651bccc1a3b2abed65366
ompidr <98680675+ompidr@users.noreply.github.com>
2022-01-31 09:59:52 +0530

    Update lichess-bot.py

--- lichess-bot.py
+++ lichess-bot.py
@@ -248,7 +248,7 @@ def play_game(li, game_id, control_queue, engine_factory, user_profile, config, 
     opponent = game.black.name if game.white.name == user_profile["username"] else game.white.name
     conversation.send_reply(SendLine('player'), f'All the best {opponent} !')
     conversation.send_reply(SendLine('player'), f'May the best man win !')
-    conversation.send_reply(SendLine('spectator'), f'Welcome to my game spectators!')
+    conversation.send_reply(SendLine('spectator'), f'Welcome to my game spectators')
     conversation.send_reply(SendLine('spectator'), f'This is a bot created by @Nightmare_123 ')
     
     logger.info("+++ {}".format(game))
